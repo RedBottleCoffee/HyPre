fb34c8719eb34db8655fd3dfb7a706d3648f664d
arabian9ts <nok14.tm59@gmail.com>
2018-10-27 10:16:14 +0900

    setup: configures

---
+++ pyproc/config/__init__.py
@@ -0,0 +1,31 @@
+from config.configure import Configure
+
+conf = Configure()
+
+#== universal
+conf.sampling_rate = 44100
+conf.duration = 1
+conf.hop_length = 347 # to make time steps 128
+conf.fmin = 20
+conf.fmax = conf.sampling_rate // 2
+conf.n_mels = 128
+conf.n_fft = conf.n_mels * 20
+conf.model = 'mobilenetv2' # 'alexnet'
+
+#== for trainer
+conf.folder = '.'
+conf.n_fold = 1
+conf.normalize = 'samplewise'
+conf.valid_limit = None
+conf.random_state = 42
+conf.test_size = 0.01
+conf.samples_per_file = 5
+conf.batch_size = 32
+conf.learning_rate = 0.0001
+conf.epochs = 500
+conf.verbose = 2
+
+#== for recognizer
+conf.rt_process_count = 1
+conf.rt_oversamples = 10
+conf.pred_ensembles = 10
