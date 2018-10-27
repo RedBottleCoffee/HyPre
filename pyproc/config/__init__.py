04589e24ae3ad5fac6b00c30b1e268af8ccf9a22
arabian9ts <nok14.tm59@gmail.com>
2018-10-27 10:53:11 +0900

    update: config for trainer

--- pyproc/config/__init__.py
+++ pyproc/config/__init__.py
@@ -13,6 +13,13 @@ conf.fmax = conf.sampling_rate // 2
 conf.n_mels = 128
 conf.n_fft = conf.n_mels * 20
 conf.model = 'mobilenetv2' # 'alexnet'
+conf.samples = conf.sampling_rate * conf.duration
+conf.num_classes = 2
+
+#== for recognizer
+conf.rt_process_count = 1
+conf.rt_oversamples = 10
+conf.pred_ensembles = 10
 
 #== for trainer
 conf.folder = '.'
@@ -27,8 +34,3 @@ conf.learning_rate = 0.0001
 conf.epochs = 500
 conf.verbose = 2
 conf.dims = (conf.n_mels, 1 + int(np.floor(conf.samples/conf.hop_length)), 1)
-
-#== for recognizer
-conf.rt_process_count = 1
-conf.rt_oversamples = 10
-conf.pred_ensembles = 10
