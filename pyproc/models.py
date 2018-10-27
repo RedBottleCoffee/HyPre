6f933e5b916bf352461188da639526c24aa36db0
arabian9ts <nok14.tm59@gmail.com>
2018-10-27 11:34:43 +0900

    fix: cnain escaping

--- pyproc/models.py
+++ pyproc/models.py
@@ -9,24 +9,24 @@ class Model:
 
     def build_alexnet(self):
         model = Modelable()
-        alexnet = model        \
-                  .input(conf.dims)     \
+        alexnet = model                           \
+                  .input(conf.dims)               \
                   .conv2d(48, 11, strides=(2, 3)) \
-                  .mpool2d(3, strides=(1, 2)) \
-                  .normalize() \
+                  .mpool2d(3, strides=(1, 2))     \
+                  .normalize()                    \
                   .conv2d(128, 5, strides=(2, 3)) \
-                  .mpool2d(3, strides=2) \
-                  .normalize() \
-                  .conv2d(192, 3, strides=(1, 2))    \
+                  .mpool2d(3, strides=2)          \
+                  .normalize()                    \
+                  .conv2d(192, 3, strides=(1, 2)) \
                   .conv2d(192, 3, strides=(1, 1)) \
                   .conv2d(128, 3, strides=(1, 1)) \
-                  .mpool2d(3, strides=(1, 2)) \
-                  .normalize() \
-                  .flat() \
-                  .dense(256) \
-                  .dropout(0.5) \
-                  .dense(256) \
-                  .dropout(0.5) \
+                  .mpool2d(3, strides=(1, 2))     \
+                  .normalize()                    \
+                  .flat()                         \
+                  .dense(256)                     \
+                  .dropout(0.5)                   \
+                  .dense(256)                     \
+                  .dropout(0.5)                   \
                   .dense(conf.num_classes, activation='softmax')
 
         return alexnet.build()
