169964e91b1f0cb0ce0c989d9defae4e38fe8425
arabian9ts <nok14.tm59@gmail.com>
2018-10-27 09:44:52 +0900

    add: chainable model constructor

---
+++ pyproc/modelable.py
@@ -0,0 +1,110 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPool2D
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+from keras.layers.core import Dense, Activation, Dropout, Flatten
+
+class Modelable:
+    def __init__(self):
+        self.stream = None
+
+    def input(
+        self,
+        shape=(32, 32, 3)
+    ):
+        self.stream = Input(shape=shape)
+        return self
+
+    def conv2d(
+        self,
+        filters=32,
+        kernel_size=(32, 32),
+        strides=(1, 1),
+        padding='same',
+        activation='relu',
+        use_bias=True,
+        kernel_initializer='random_normal',
+        bias_initializer='zeros'
+    ):
+        self.stream = Conv2D(
+            filters=filters,
+            kernel_size=kernel_size,
+            strides=strides,
+            padding=padding,
+            activation=activation,
+            use_bias=use_bias,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer
+          ) (self.stream)
+        return self
+
+    def mpool2d(
+        self,
+        pool_size=(2, 2),
+        strides=(2, 2),
+        padding='same'
+    ):
+        self.stream = MaxPool2D(
+            pool_size=pool_size,
+            strides=strides,
+            padding=padding
+          ) (self.stream)
+        return self
+
+    def flat(
+        self
+    ):
+        self.stream = Flatten() (self.stream)
+        return self
+
+    def fc(
+        self,
+        units=10,
+        activation='relu',
+        use_bias=True,
+        kernel_initializer='glorot_uniform',
+        bias_initializer='zeros'
+    ):
+        self.stream = Dence(
+            units=units,
+            activation=activation,
+            use_bias=use_bias,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer
+        ) (self.stream)
+        return self
+
+    def normalize(
+        self,
+        axis=-1,
+        momentum=0.99,
+        epsilon=0.001,
+        center=True,
+        scale=True,
+        beta_initializer='zeros',
+        gamma_initializer='ones',
+        moving_mean_initializer='zeros',
+        moving_variance_initializer='ones',
+        beta_regularizer=None,
+        gamma_regularizer=None,
+        beta_constraint=None,
+        gamma_constraint=None
+    ):
+        self.stream = BatchNormalization(
+            axis=-axis,
+            momentum=momentum,
+            epsilon=epsilon,
+            center=center,
+            scale=scale,
+            beta_initializer=beta_initializer,
+            gamma_initializer=gamma_initializer,
+            moving_mean_initializer=moving_mean_initializer,
+            moving_variance_initializer=moving_variance_initializer,
+            beta_regularizer=beta_regularizer,
+            gamma_regularizer=gamma_regularizer,
+            beta_constraint=beta_constraint,
+            gamma_constraint=gamma_constraint
+        ) (self.stream)
+        return self
