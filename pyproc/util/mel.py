4ef510534c9b5487e8946a35181518e932bf60b7
arabian9ts <nok14.tm59@gmail.com>
2018-10-27 17:25:52 +0900

    fix: check mel shape

--- pyproc/util/mel.py
+++ pyproc/util/mel.py
@@ -4,14 +4,22 @@ import librosa
 
 def _load_wave(conf, path):
     data = librosa.load(path, sr=conf.sampling_rate)
-    return np.asarray(data[0][:conf.samples * conf.duration])
+    cut_sound = np.asarray(data[0][:conf.samples * conf.duration])
+    if len(cut_sound) == conf.samples * conf.duration:
+        return cut_sound
+    else:
+        return None
 
-def load_wavs(conf, paths):
+def load_wavs(conf, pathsets):
     wavs = []
-    for path in paths:
-        wav = _load_wave(conf, path)
-        wavs.append(wav)
-    return wavs
+    loaded_paths, loaded_labels = [], []
+    for pathset in pathsets:
+        wav = _load_wave(conf, pathset[0])
+        if wav is not None:
+          wavs.append(wav)
+          loaded_paths.append(pathset[0])
+          loaded_labels.append(pathset[1])
+    return wavs, loaded_paths, loaded_labels
 
 def _sound_to_mel(conf, array):
     melspectrogram = librosa.feature.melspectrogram(array,
@@ -24,12 +32,15 @@ def _sound_to_mel(conf, array):
     melspectrogram = librosa.power_to_db(melspectrogram)
     melspectrogram = melspectrogram.astype(np.float32)
 
-    return np.reshape(melspectrogram, (128, 128, 1))
+    if melspectrogram.shape == (128, 128):
+        return np.reshape(melspectrogram, (128, 128, 1))
+
+    return None
 
 def sounds_to_mels(conf, sound_array):
     mels = []
     for sound in sound_array:
         mel = _sound_to_mel(conf, sound)
-        mel = np.reshape(mel, (128, 128, 1))
-        mels.append(mel)
+        if mel is not None:
+            mels.append(mel)
     return mels
