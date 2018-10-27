ba7b6bafb1c7c6ac605e6e8bbf7cbc3301fbddca
arabian9ts <nok14.tm59@gmail.com>
2018-10-27 10:25:46 +0900

    update: config variables

--- pyproc/config/__init__.py
+++ pyproc/config/__init__.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from config.configure import Configure
 
 conf = Configure()
@@ -24,6 +26,7 @@ conf.batch_size = 32
 conf.learning_rate = 0.0001
 conf.epochs = 500
 conf.verbose = 2
+conf.dims = (conf.n_mels, 1 + int(np.floor(conf.samples/conf.hop_length)), 1)
 
 #== for recognizer
 conf.rt_process_count = 1
