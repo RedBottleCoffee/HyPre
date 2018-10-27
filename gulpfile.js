5ce2a060a913d586eef18a01151c30a1f3eac175
Tomoki Ichikawa <ichikawa1060@gmail.com>
2018-10-27 10:21:45 +0900

    LiveReload

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,7 @@ var electron = require('electron-connect').server.create();
 
 gulp.task('start', function() {
   electron.start('./app')
+
+  gulp.watch(['./app/*.js'], electron.restart);
+  gulp.watch(['./app/*.{html,css}'], electron.reload)
 })
