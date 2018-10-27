6bffa71428deeb60981d09431f6b57cbd5b6a169
arabian9ts <nok14.tm59@gmail.com>
2018-10-27 16:07:46 +0900

    fix: mel shape

--- pyproc/util/mel.py
+++ pyproc/util/mel.py
@@ -2,11 +2,18 @@ import numpy as np
 import librosa
 
 
-def load_wave(conf, path):
+def _load_wave(conf, path):
     data = librosa.load(path, sr=conf.sampling_rate)
     return np.asarray(data[0][:conf.samples * conf.duration])
 
-def sound_to_mels(conf, array):
+def load_wavs(conf, paths):
+    wavs = []
+    for path in paths:
+        wav = _load_wave(conf, path)
+        wavs.append(wav)
+    return wavs
+
+def _sound_to_mel(conf, array):
     melspectrogram = librosa.feature.melspectrogram(array,
                                                  sr=conf.sampling_rate,
                                                  n_mels=conf.n_mels,
@@ -17,4 +24,12 @@ def sound_to_mels(conf, array):
     melspectrogram = librosa.power_to_db(melspectrogram)
     melspectrogram = melspectrogram.astype(np.float32)
 
-    return melspectrogram
+    return np.reshape(melspectrogram, (128, 128, 1))
+
+def sounds_to_mels(conf, sound_array):
+    mels = []
+    for sound in sound_array:
+        mel = _sound_to_mel(conf, sound)
+        mel = np.reshape(mel, (128, 128, 1))
+        mels.append(mel)
+    return mels
