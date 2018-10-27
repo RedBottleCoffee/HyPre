af6bf7e387982b7acf8fa2e765dc2078af2a7d4a
Kansei Kogami <kk7221dev@gmail.com>
2018-10-27 11:40:26 +0900

    fix:  to ''

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var sassGlob = require("gulp-sass-glob");
-var babel = require("gulp-babel");
+var sassGlob = require('gulp-sass-glob');
+var babel = require('gulp-babel');
 var electron = require('electron-connect').server.create();
 
 var srcDir = './app/src';
@@ -12,8 +12,8 @@ gulp.task('start', ['compile'], function() {
 
   gulp.watch([srcDir + '/js/**/*.js'], ['compile']);
   gulp.watch(['./app/index.js'], electron.restart);
-  gulp.watch(['./app/src/scss/*.scss'], ['sass']);
-  gulp.watch(['./app/dist/*.css'], electron.reload);
+  gulp.watch([srcDir + '/scss/*.scss'], ['sass']);
+  gulp.watch([distDir + '/css/*.css'], electron.reload);
   gulp.watch(['./app/*.html'], electron.reload);
 });
 
@@ -24,8 +24,8 @@ gulp.task('compile', function(){
 });
 
 gulp.task('sass', function(){
-  gulp.src('./app/src/scss/style.scss')
+  gulp.src(srcDir + '/scss/style.scss')
     .pipe(sassGlob())
     .pipe(sass())
-    .pipe(gulp.dest('./app/dist/'));
+    .pipe(gulp.dest(distDir + '/css/'));
 });
