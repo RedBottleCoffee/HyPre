9999e5941bcd20e70f80b9eb65a7f6ee64f89267
arabian9ts <nok14.tm59@gmail.com>
2018-10-27 11:34:15 +0900

    fix: indents

--- pyproc/modelable.py
+++ pyproc/modelable.py
@@ -14,25 +14,23 @@ class Modelable:
     def build(self):
         return Model(inputs=self.root, outputs=self.stream)
 
-    def input(
-        self,
-        shape=(32, 32, 3)
-    ):
+    def input(self,
+              shape=(32, 32, 3)):
+
         self.stream = Input(shape=shape)
         self.root = self.root or self.stream
         return self
 
-    def conv2d(
-        self,
-        filters=32,
-        kernel_size=(32, 32),
-        strides=(1, 1),
-        padding='same',
-        activation='relu',
-        use_bias=True,
-        kernel_initializer='random_normal',
-        bias_initializer='zeros'
-    ):
+    def conv2d(self,
+               filters=32,
+               kernel_size=(32, 32),
+               strides=(1, 1),
+               padding='same',
+               activation='relu',
+               use_bias=True,
+               kernel_initializer='random_normal',
+               bias_initializer='zeros'):
+
         self.stream = Conv2D(
             filters=filters,
             kernel_size=kernel_size,
@@ -45,12 +43,11 @@ class Modelable:
           ) (self.stream)
         return self
 
-    def mpool2d(
-        self,
-        pool_size=(2, 2),
-        strides=(2, 2),
-        padding='same'
-    ):
+    def mpool2d(self,
+                pool_size=(2, 2),
+                strides=(2, 2),
+                padding='same'):
+
         self.stream = MaxPool2D(
             pool_size=pool_size,
             strides=strides,
@@ -58,29 +55,25 @@ class Modelable:
           ) (self.stream)
         return self
 
-    def gavgpool2d(
-        self,
-        data_format=None
-    ):
+    def gavgpool2d(self,
+                   data_format=None):
+
         self.stream = GlobalAveragePooling2D(
             data_format=data_format
         ) (self.stream)
         return self
 
-    def flat(
-        self
-    ):
+    def flat(self):
         self.stream = Flatten() (self.stream)
         return self
 
-    def fc(
-        self,
-        units=10,
-        activation='relu',
-        use_bias=True,
-        kernel_initializer='glorot_uniform',
-        bias_initializer='zeros'
-    ):
+    def fc(self,
+           units=10,
+           activation='relu',
+           use_bias=True,
+           kernel_initializer='glorot_uniform',
+           bias_initializer='zeros'):
+
         self.stream = Dence(
             units=units,
             activation=activation,
@@ -90,22 +83,21 @@ class Modelable:
         ) (self.stream)
         return self
 
-    def normalize(
-        self,
-        axis=-1,
-        momentum=0.99,
-        epsilon=0.001,
-        center=True,
-        scale=True,
-        beta_initializer='zeros',
-        gamma_initializer='ones',
-        moving_mean_initializer='zeros',
-        moving_variance_initializer='ones',
-        beta_regularizer=None,
-        gamma_regularizer=None,
-        beta_constraint=None,
-        gamma_constraint=None
-    ):
+    def normalize(self,
+                  axis=-1,
+                  momentum=0.99,
+                  epsilon=0.001,
+                  center=True,
+                  scale=True,
+                  beta_initializer='zeros',
+                  gamma_initializer='ones',
+                  moving_mean_initializer='zeros',
+                  moving_variance_initializer='ones',
+                  beta_regularizer=None,
+                  gamma_regularizer=None,
+                  beta_constraint=None,
+                  gamma_constraint=None):
+
         self.stream = BatchNormalization(
             axis=-axis,
             momentum=momentum,
@@ -123,19 +115,18 @@ class Modelable:
         ) (self.stream)
         return self
 
-    def dense(
-        self,
-        units,
-        activation='relu',
-        use_bias=True,
-        kernel_initializer='glorot_uniform',
-        bias_initializer='zeros',
-        kernel_regularizer=None,
-        bias_regularizer=None,
-        activity_regularizer=None,
-        kernel_constraint=None,
-        bias_constraint=None
-     ):
+    def dense(self,
+              units,
+              activation='relu',
+              use_bias=True,
+              kernel_initializer='glorot_uniform',
+              bias_initializer='zeros',
+              kernel_regularizer=None,
+              bias_regularizer=None,
+              activity_regularizer=None,
+              kernel_constraint=None,
+              bias_constraint=None):
+
         self.stream = Dense(
             units=units,
             activation=activation,
@@ -150,12 +141,11 @@ class Modelable:
         ) (self.stream)
         return self
 
-    def dropout(
-        self,
-        rate=0.5,
-        noise_shape=None,
-        seed=None
-    ):
+    def dropout(self,
+                rate=0.5,
+                noise_shape=None,
+                seed=None):
+
         self.stream = Dropout(
             rate=rate,
             noise_shape=noise_shape,
