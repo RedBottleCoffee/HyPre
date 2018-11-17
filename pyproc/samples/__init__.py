dc6d9a71e36a65db57aa21795d26fa565850876f
arabian9ts <nok14.tm59@gmail.com>
2018-11-17 15:58:57 +0900

    fix: var scope

--- pyproc/samples/__init__.py
+++ pyproc/samples/__init__.py
@@ -5,7 +5,7 @@ from config import *
 file_path_list = glob("samples/**/*")
 
 #== ディレクトリから音声をカテゴライズ
-def categorize(file_path_list):
+def _categorize(file_path_list):
     labels = conf.labels
     catg_dict = dict([(label.lower(), []) for label in labels])
     for path in file_path_list:
@@ -14,21 +14,21 @@ def categorize(file_path_list):
     return catg_dict
 
 #== One-Hot表現生成
-def one_hot(i, size):
+def _one_hot(i, size):
     return list(np.diag([1]*size)[i])
 
 #== ファイル名と正解ラベル(One-Hot)の対応づけ
-def name_and_category(paths, label):
-    return dict(zip(paths, [one_hot(label, conf.num_classes)] * len(paths)))
+def _name_and_category(paths, label):
+    return dict(zip(paths, [_one_hot(label, conf.num_classes)] * len(paths)))
 
 
 
-categories = categorize(file_path_list)
-backet = {}
+_categories = _categorize(file_path_list)
+_backet = {}
 
-for k, v in categories.items():
-    spec = name_and_category(v, conf.labels.index(k))
-    backet = {**backet, **spec}
+for k, v in _categories.items():
+    spec = _name_and_category(v, conf.labels.index(k))
+    _backet = {**_backet, **spec}
 
 
-dataset = backet
+dataset = _backet
